dcc403ca73e2c3c231587b4b3aad8da2733559cd
王聖崴 <shengwei.860126@gmail.com>
2023-02-24 20:25:48 +0800

    Spring boot project

---
+++ src/test/java/com/springboot/test/User/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.test.User;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
